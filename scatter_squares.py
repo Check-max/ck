ee062f8882ba7b2c5f4c58c58e59de054d449c79
Gleen <ck2240226545@icloud.com>
2020-03-22 19:57:25 +0800

    自动计算数据

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-x_value = [1, 2, 3, 4, 5, 6]
+x_value = list(range(1, 1001))
 y_value = []
 for a in x_value:
     y_value.append(a * a)
-plt.scatter(x_value, y_value, s=200)
+plt.scatter(x_value, y_value, s=2)
 # 设置图标标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=25)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of value", fontsize=14)
-# 设置刻度标记的大小
-plt.tick_params(axis='both', which='major', labelsize=14)
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
 plt.show()
