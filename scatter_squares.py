9483765ccba5606f12936e584ca3294b245635b8
Gleen <ck2240226545@icloud.com>
2020-03-22 19:50:02 +0800

    绘制五个点

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_value = [1, 2, 3, 4, 5, 6]
+y_value = []
+for a in x_value:
+    y_value.append(a * a)
+plt.scatter(x_value, y_value, s=200)
 # 设置图标标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=25)
 plt.xlabel("Value", fontsize=14)
